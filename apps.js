c6e8ccb732dc927c7e609414abcd99ca7944dc28
meronan <134378150+MeronKR@users.noreply.github.com>
2023-10-11 20:29:57 +0800

    add skeleton + style

---
+++ apps.js
@@ -0,0 +1,24 @@
+
+/* const myLibrary = [];
+
+function Book(title, author, pages){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = "UNREAD"
+ }
+
+ Book.prototype.info = function(){
+    let info = `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+    console.log(info)
+ }
+
+ Book.prototype.addToLibrary = function(){
+    myLibrary.push(this)
+    return myLibrary
+ }
+
+ let book = new Book("The Hobbit", "J.R.R Tolkein", 295)
+ let book2 = new Book("Pride & Prejudice", "Jane Austen", 490)
+ let book3 = new Book("To Kill a Mockingbird", "Harper Lee", 384)
+ let book4 = new Book("The Book Thief", "Markus Zusak", 592) */
